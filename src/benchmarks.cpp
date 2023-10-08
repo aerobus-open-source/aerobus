457267b17716ffb8ba4718a8a419b3138f4cf2ab
regis portalez <1440561+JeWaVe@users.noreply.github.com>
2023-10-08 12:22:21 +0200

    fix benchmarks

--- src/benchmarks.cpp
+++ src/benchmarks.cpp
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
 #include <cstdio>
 #include <cstdlib>
 #include <chrono>
 #include "lib.h"
+
 
 INLINED
 double expm1_12(const double x) {
@@ -11,61 +11,18 @@ double expm1_12(const double x) {
 		V::eval(V::eval(V::eval(V::eval(V::eval(V::eval(x))))))))))));
 }
 
+template<size_t N>
 INLINED
-void vexpm1_12(double * in, double * out, size_t N) {
+void vexpm1_12(double * in, double * out) {
+	static_assert(N % 32 == 0);
 	in = (double*) __builtin_assume_aligned(in, 64);
 	out = (double*) __builtin_assume_aligned(out, 64);
 #pragma omp parallel for
-	for (int i = 0; i < N; ++i) {
-		out[i] = expm1_12(in[i]);
-	}
-}
-
-int main() {
-	const size_t N = 100000000;
-	double* in = static_cast<double*>(aligned_alloc(64, N * sizeof(double)));
-	double* out = static_cast<double*>(aligned_alloc(64, N * sizeof(double)));
-	for(int i = 0; i < N; ++i) {
-		in[i] = 0.1;
-	}
-	// warmup
-	vexpm1_12(in, out, N);
-	vexpm1_12(in, out, N);
-	vexpm1_12(in, out, N);
-	double best = 1.0E9;
-	for (int i = 0; i < 10; ++i) {
-		auto start = std::chrono::steady_clock::now();
-		vexpm1_12(in, out, N);
-		auto stop = std::chrono::steady_clock::now();
-		std::chrono::duration<double> time = stop - start;
-		if (time.count() < best) {
-			best = time.count();
+	for (int i = 0; i < N; i += 32) {
+		#pragma simd
+		for(int j = i; j < i+32; ++j) {
+			out[j] = expm1_12(in[j]);
 		}
-	}
-
-	printf("time for 100E6 exp12 : %lf\n", best);
-	printf("achieved Gflops : %lf\n", ((double)N * 13.0 * 12.0E-9 / best));
-=======
-#include <cstdio>
-#include <cstdlib>
-#include <chrono>
-#include "lib.h"
-
-
-INLINED
-double expm1_12(const double x) {
-	using V = aerobus::expm1<aerobus::i64, 13>;
-	return V::eval(V::eval(V::eval(V::eval(V::eval(V::eval(
-		V::eval(V::eval(V::eval(V::eval(V::eval(V::eval(x))))))))))));
-}
-
-INLINED
-void vexpm1_12(double * in, double * out, size_t N) {
-	in = (double*) __builtin_assume_aligned(in, 64);
-	out = (double*) __builtin_assume_aligned(out, 64);
-#pragma omp parallel for
-	for (int i = 0; i < N; ++i) {
-		out[i] = expm1_12(in[i]);
 	}
 }
 
@@ -77,13 +34,13 @@ int main() {
 		in[i] = 0.1;
 	}
 	// warmup
-	vexpm1_12(in, out, N);
-	vexpm1_12(in, out, N);
-	vexpm1_12(in, out, N);
+	vexpm1_12<N>(in, out);
+	vexpm1_12<N>(in, out);
+	vexpm1_12<N>(in, out);
 	double best = 1.0E9;
 	for (int i = 0; i < 10; ++i) {
 		auto start = std::chrono::steady_clock::now();
-		vexpm1_12(in, out, N);
+		vexpm1_12<N>(in, out);
 		auto stop = std::chrono::steady_clock::now();
 		std::chrono::duration<double> time = stop - start;
 		if (time.count() < best) {
@@ -93,5 +50,4 @@ int main() {
 
 	printf("time for 100E6 exp12 : %lf\n", best);
 	printf("achieved Gflops : %lf\n", ((double)N * 13.0 * 12.0E-9 / best));
->>>>>>> d26c25a (merge headers back into one big file)
 }
